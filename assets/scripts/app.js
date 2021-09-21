575cbea29c012970d0c9b345f3140f87528ba9d7
Bealby <mgbealby@gmail.com>
2021-09-21 15:35:31 +0200

    Add else if JS for win/ lose/ draw

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10; // Capitals becuase global value
+const MONSTER_ATTACK_VALUE = 14; // Capitals becuase global value
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
@@ -9,6 +10,15 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler() {
     const damage = dealMonsterDamage(ATTACK_VALUE);
     currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
+    currentPlayerHealth -= playerDamage;
+    if (currentMonsterHealth <= 0 && currentPlayerHeatlh > 0) {
+        alert('You Won!');
+    } else if (currentPlayerHealth <= 0 && currentPlayerHeatlh > 0) {
+        alert('You Lose!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHeatlh <= 0) {
+        alert('Draw!');
+    }
 }
 
 attackBtn.addEventListener('click', attackHandler);
